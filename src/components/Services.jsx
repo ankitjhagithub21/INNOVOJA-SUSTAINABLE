4a179b64a6bfb41e6bda65c45f8a9f05e976a97d
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-17 00:40:56 +0530

    added services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,29 +3,33 @@ import React from 'react'
 const Services = () => {
   return (
    <section className='bg-[#2A304B] py-12 px-5'>
-     <div className='w-full'>
+     <div className='container mx-auto'>
         <h1 className='lg:text-5xl text-3xl font-bold text-center'>How Do We Do That?</h1>
         <p className='text-center my-10'>By creating the most durable and innovative ion exchange membranes and polymers:</p>
        
-      </div>
-      <div className='flex lg:w-2/3 mx-auto m flex-wrap items-center justify-center'>
-        <div className='w-1/2   '>
-            <img src="https://ionomr.com/wp-content/uploads/2018/10/ionomr-aemion-crop-1-800x878.jpg" alt="" width={300}/>
-            <div>
-            <h2>Aemion+™</h2>
+       <div className='flex flex-wrap lg:w-2/3 w-full mx-auto'>
+      
+        <div className='md:w-1/2 w-full p-5'>
+            <img src="https://ionomr.com/wp-content/uploads/2018/10/ionomr-aemion-crop-1-800x878.jpg" alt="" />
+            <div className='bg-[#F4965D] p-5 flex flex-col items-start  gap-5'>
+            <h2 className='hover:text-[#2A304B] cursor-pointer font-bold text-xl items-start transition duration-500'>Aemion+™</h2>
             <p>The most durable and high-performance Anion Exchange Membrane (AEM) on the market – unlocks high temperature alkaline electrochemistry which increases efficiency and decreases capital costs (CAPEX).</p>
-            <button>Learn more</button>
+            <button className='px-4 py-2 bg-[#2A304B] hover:bg-[#7DA2C7] transition duration-500 rounded-full'>Learn more</button>
             </div>
         </div>
-        <div className='w-1/2'>
-            <img src="https://ionomr.com/wp-content/uploads/2018/10/ionomr-aemion-crop-1-800x878.jpg" alt="" width={300}/>
-            <div>
-            <h2>Aemion+™</h2>
-            <p>The most durable and high-performance Anion Exchange Membrane (AEM) on the market – unlocks high temperature alkaline electrochemistry which increases efficiency and decreases capital costs (CAPEX).</p>
-            <button>Learn more</button>
+        
+        <div className='md:w-1/2 w-full p-5'>
+            <img src="https://ionomr.com/wp-content/uploads/2018/10/ionomr-pemion-crop-1-800x878.jpg" alt="" />
+            <div className='bg-[#F4965D] p-5 flex flex-col items-start  gap-5'>
+            <h2 className='hover:text-[#2A304B] cursor-pointer font-bold text-xl items-start transition duration-500'>Pemion™</h2>
+            <p>Breakthrough hydrocarbon Proton Exchange Membrane (PEM) – provides considerably higher performance, durability and lower gas crossover while eliminating environmental concerns of Fluorine.</p>
+            <button className='px-4 py-2 bg-[#2A304B] hover:bg-[#7DA2C7] transition duration-500 rounded-full'>Learn more</button>
             </div>
         </div>
+       </div>
+
       </div>
+    
    </section>
   )
 }
