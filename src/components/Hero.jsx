51466c308334c7e38730c9244fdf976f0528a21c
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-17 00:07:52 +0530

    added overlay

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section className='md:h-screen h-fit w-full'>
-            <div className='overlay flex items-center h-fit md:h-screen py-24 mt-24 md:mt-0 justify-center relative p-5'>
+        <section className='md:h-screen h-fit w-full flex items-center justify-center relative'>
+
             <video
                 className='absolute top-0 left-0 w-full h-full object-cover'
                 src="./video.mp4"
@@ -12,8 +12,11 @@ const Hero = () => {
                 loop
             ></video>
 
-            <div className='relative flex flex-col gap-3 text-white z-10 text-center'>
-                <h1 className='lg:text-6xl md:text-3xl text-xl lowercase font-bold'>
+            {/* Overlay */}
+            <div className='absolute top-0 left-0 w-full h-full bg-black opacity-30'></div>
+
+            <div className='relative flex flex-col gap-3 text-white z-10 text-center hover:scale-105 transition'>
+                <h1 className='lg:text-6xl md:text-3xl text-xl lowercase font-bold '>
                     INNOVOJA SUSTAINABLE SOLUTIONS
                 </h1>
                 <p className='lg:text-3xl text-sm'>
@@ -23,11 +26,8 @@ const Hero = () => {
                     advanced materials to unlock hydrogen & carbon-neutral technologies
                 </p>
             </div>
-            </div>
-           
 
         </section>
-
     )
 }
 
