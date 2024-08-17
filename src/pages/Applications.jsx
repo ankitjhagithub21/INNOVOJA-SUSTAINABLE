d830e84888c3275da4739d79776376926e03a804
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-17 08:29:27 +0530

    fix applications page

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -16,7 +16,7 @@ const Applications = () => {
     : applications.filter(app => app.category === selectedCategory);
 
   return (
-    <section className='py-24 lg:px-5 px-0'>
+    <section className='lg:px-5 px-0'>
       <h1 className='lg:text-5xl text-3xl font-bold text-center lg:mt-10 md:mt-20 mt-0'>Applications</h1>
       
       <div className='flex flex-wrap items-center justify-center my-10 md:gap-5 gap-2 text-lg'>
