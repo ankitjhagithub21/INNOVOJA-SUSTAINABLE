858d35baffb6804efc8163bb2f49b2ee197bcb9f
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-17 07:21:28 +0530

    fix more info page

--- src/pages/MoreInfo.jsx
+++ src/pages/MoreInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MoreInfo = () => {
   return (
     <section className='py-24'>
-     <div className='lg:w-2/3 mx-auto py-12'>
+     <div className='lg:w-2/3 mx-auto py-12 px-5'>
      <h1 className='md:text-5xl text-3xl font-bold mt-5 text-center'>Get More Info</h1>
       <p className='text-center mt-2'>Please complete the following form so we can provide you with the most relevant information for your application.
       Note: we require company email contact. Personal email address may not be accepted.</p>
