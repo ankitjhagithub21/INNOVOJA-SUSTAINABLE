3d49a25ed26dfd67c84efc0a3e3e31c91ab6ebec
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-18 07:10:54 +0530

    fix team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -7,16 +7,16 @@ const Team = () => {
   },[])
   return (
     <section>
-    <div className="container px-5 py-24 mx-auto">
+    <div className="container lg:px-5 px-0 py-24 mx-auto">
     <h1 className="lg:text-5xl text-3xl  md:mt-20 lg:mt-5 mt-5 mb-5 text-center">
           Our Team
         </h1>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap text-center">
       
         <div className='lg:w-1/3 md:w-1/2 w-full p-5'>
             <div className='shadow-lg cursor-pointer'>
               <img src="https://ionomr.com/wp-content/uploads/2024/03/Bill-Haberlin-scaled-e1710087864260-812x820.jpg" alt="member" className='hover:opacity-80'/>
-            <div className='flex flex-col gap-2 p-10 items-center bg-white text-[var(--clr-dark)]'>
+            <div className='flex flex-col gap-2 lg:p-10 p-5 items-center bg-white text-[var(--clr-dark)]'>
             <h1 className='text-2xl font-bold'>Bill Haberlin</h1>
               <p>Chief Executive Officer & Director</p>
               <a href="#" target='_blank' className='bg-[var(--clr-dark)] text-white p-3 rounded-full'>
@@ -28,7 +28,7 @@ const Team = () => {
         <div className='lg:w-1/3 md:w-1/2 w-full p-5'>
             <div className='shadow-lg cursor-pointer'>
               <img src="https://ionomr.com/wp-content/uploads/2024/03/Morris-Tan-scaled-e1710087490241-812x820.jpg" alt="member" className='hover:opacity-80'/>
-            <div className='flex flex-col gap-2 p-10 items-center bg-white text-[var(--clr-dark)]'>
+            <div className='flex flex-col gap-2 lg:p-10 p-5 items-center bg-white text-[var(--clr-dark)]'>
             <h1 className='text-2xl font-bold'>Moris Tan</h1>
               <p>Chief Executive Officer & Director</p>
               <a href="#" target='_blank' className='bg-[var(--clr-dark)] text-white p-3 rounded-full'>
@@ -40,7 +40,7 @@ const Team = () => {
         <div className='lg:w-1/3 md:w-1/2 w-full p-5'>
             <div className='shadow-lg cursor-pointer'>
               <img src="https://ionomr.com/wp-content/uploads/2024/05/IMG_6066_1-concentrate-812x820.jpg" alt="member" className='hover:opacity-80'/>
-            <div className='flex flex-col gap-2 p-10 items-center bg-white text-[var(--clr-dark)]'>
+            <div className='flex flex-col gap-2 lg:p-10 p-5 items-center bg-white text-[var(--clr-dark)]'>
             <h1 className='text-2xl font-bold'>Benjman Britton</h1>
               <p>Chief Executive Officer & Director</p>
               <a href="#" target='_blank' className='bg-[var(--clr-dark)] text-white p-3 rounded-full'>
