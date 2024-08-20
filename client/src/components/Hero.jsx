9b95b08a118c9ba2e8715c58d7041794c5d7a2a2
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-20 06:08:03 +0530

    fix hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section className='hero w-full lg:h-screen md:h-[50vh] h-[30vh]   flex items-center justify-center relative'>
+        <section className='hero w-full lg:h-screen h-[40vh]    flex items-center justify-center relative'>
 
             <video
                 className='absolute top-0 left-0 w-full h-full  object-cover'
@@ -13,7 +13,7 @@ const Hero = () => {
             ></video>
 
            
-            <div className='relative flex flex-col gap-3 px-5 py-24 mt-24  lg:mt-0 text-white z-10 text-center hover:scale-105 transition'>
+            <div className='relative flex flex-col gap-1 px-5  md:mt-24 mt-12 lg:mt-0 text-white z-10 text-center hover:scale-105 transition'>
              <h2 className='lg:text-5xl md:text-xl text-sm font-bold'>INNOVOJA SUSTAINABLE SOLUTIONS</h2>
                 <p className='lg:text-3xl text-sm'>
                     enables the global renewable transition with
