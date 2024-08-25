76f0cfc43abacc597015204ca61e1468b8f72bc6
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-25 16:54:16 +0530

    added loader

--- admin/src/components/Loader.jsx
+++ admin/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='h-screen w-full flex items-center justify-center'>
-      <span>Loader</span>
+      <span className='loader'>Loader</span>
     </div>
   )
 }
