62a095bc9d88ee7e31a580b54df0b2d77e5390be
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-25 09:00:05 +0530

    fix team page

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -20,8 +20,8 @@ const Team = () => {
         {
           members.map((member) => {
             return <div className='lg:w-1/3 md:w-1/2 w-full p-5' key={member._id}>
-              <div className='shadow-lg cursor-pointer bg-white'>
-                <img src={member.profilePic} alt="member" className='hover:opacity-80' />
+              <div className='shadow-lg cursor-pointer '>
+                <img src={member.profilePic} alt="member" className='hover:opacity-80 object-contain w-full ' />
                 <div className='flex flex-col gap-2 lg:p-10 p-5 items-center bg-white text-[var(--clr-dark)]'>
                   <h1 className='text-2xl font-bold'>{member.name}</h1>
                   <p>{member.role}</p>
